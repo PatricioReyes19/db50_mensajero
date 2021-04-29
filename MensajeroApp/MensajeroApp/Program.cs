c3589a2d7bc04c3aaa01a92c5b06d83ec61f1cdf
Patricio Reyes <reyes.patricio.p@gmail.com>
2021-04-29 11:41:53 -0400

    generamos la percistencia en archivos, DAL,Factory y singleton

---
+++ MensajeroApp/MensajeroApp/Program.cs
@@ -0,0 +1,79 @@
+﻿using MensajeroModel.DAL;
+using MensajeroModel.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MensajeroApp
+{
+    class Program
+    {
+        //1. crear menu
+        //2. dos metodos IngresarMensajes y MostrarMensajes
+        //3. Al ingresar un mensaje definir el tipo como aplicacion
+
+        static IMensajesDAL dal = MensajesDALFactory.CreateDal();
+
+        static void IngresarMensaje()
+        {
+            string nombre, detalle, tipo;
+            do
+            {
+                Console.WriteLine("Ingrese el nombre del remitente");
+                nombre = Console.ReadLine().Trim();
+            } while (nombre == string.Empty);
+
+            do
+            {
+                Console.WriteLine("Ingrese mensaje: ");
+                detalle = Console.ReadLine().Trim();
+            } while (detalle == string.Empty || detalle.Length > 20);
+
+            Mensaje m = new Mensaje()
+            {
+                Nombre = nombre,
+                Detalle = detalle,
+                Tipo = "Aplicacion"
+            };
+            dal.Save(m);
+        }
+
+        static void MostrarMensaje()
+        {
+            List<Mensaje> mensajes = dal.GetAll();
+            mensajes.ForEach(m =>
+            {
+            Console.WriteLine("Nombre:{0} Detalle:{1} Tipo:{2}"
+                , m.Nombre, m.Detalle, m.Tipo);
+            });
+        }
+
+        static bool Menu()
+        {
+            bool continuar = true;
+            Console.WriteLine("1. Ingresar Mensaje");
+            Console.WriteLine("2. Mostrar Mensaje");
+            Console.WriteLine("0. salir");
+            
+            String opcion = Console.ReadLine().Trim();
+            switch (opcion)
+            {
+                case "1": IngresarMensaje();
+                    break;
+                case "2": MostrarMensaje();
+                    break;
+                case "0": continuar = false;
+                    break;
+                default: Console.WriteLine("Porfavor no se haga el larry");
+                    break;
+            }
+            return continuar;
+        }
+        static void Main(string[] args)
+        {
+            while (Menu()) ;
+        }
+    }
+}
---
+++ MensajeroApp/MensajeroModel/DAL/MensajesDALArchivos.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MensajeroModel.DTO;
+
+namespace MensajeroModel.DAL
+{
+    public class MensajesDALArchivos : IMensajesDAL
+    {
+        //patron de diseño Singleton
+        //1. constructor privado
+        private MensajesDALArchivos()
+        {
+
+        }
+        //2. una referencia estatica asi mismo
+        private static IMensajesDAL instancia;
+        //3. un metodo estatico que sea el unico que permite acceder a la instancia
+        public static IMensajesDAL GetInstancia()
+        {
+            if (instancia == null)
+                instancia = new MensajesDALArchivos();
+            return instancia;
+        }
+
+        private string archivo = Directory.GetCurrentDirectory()
+            + Path.DirectorySeparatorChar + "mensaje.csv";
+
+        public List<Mensaje> GetAll()
+        {
+            List<Mensaje> mensajes = new List<Mensaje>();
+            try
+            {
+
+                using(StreamReader reader = new StreamReader(archivo))
+                {
+                    string texto = null;
+                    do
+                    {
+                        texto = reader.ReadLine();
+                        if(texto != null)
+                        {
+                            //parsear el mensaje
+                            String[] textoArray = texto.Split(';');
+                            Mensaje m = new Mensaje()
+                            {
+                                Nombre = textoArray[0],
+                                Detalle = textoArray[1],
+                                Tipo = textoArray[2]
+                            };
+                            mensajes.Add(m);
+                        }
+                    } while (texto != null);
+                }
+             
+            }catch(IOException ex)
+            {
+
+            }
+            return mensajes;
+        }
+
+        public void Save(Mensaje m)
+        {
+            try
+            {
+                using(StreamWriter writer = new StreamWriter(archivo, true))
+                {
+                    writer.WriteLine(m);
+                    writer.Flush();
+                }
+            }catch(IOException ex)
+            {
+
+            }
+        }
+    }
+}
